5155089defde7c0175ab4cb1f35ae3dad88edf34
Filip Ceglik <fceglik@gmail.com>
2017-09-04 16:46:32 +0200

    Initial

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            slidey.Value = 0.5;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e) => jebany.Text = String.Format("Value is {0:F2}", e.NewValue);
+    }
+}
